91d41b5d8c3b494ee6def59a09de98effbb431ff
edoardo <edoardo.marangone@uniroma1.it>
2024-05-26 18:21:58 +0200

    sh file clientfile fix

--- architecture/client_files.sh
+++ architecture/client_files.sh
@@ -1,8 +1,49 @@
-message_id='9927996960327668857'
-reader_address='0xf78F10d7CeC746B85CECb44e66875618d15F9e43'
-slice_id='3877031780826419281'
-output_folder='files/prova/'
+#!/bin/sh
 
-python3 client.py --handshake --reader_address $reader_address
-python3 client.py -gs --message_id $message_id --reader_address $reader_address 
-python3 client.py -af --message_id $message_id --reader_address $reader_address --slice_id $slice_id --output_folder $output_folder
+# Initialize variables
+reader_address=""
+message_id=""
+slice_id=""
+output_folder=""
+
+# Parse command-line arguments
+while [ $# -gt 0 ]; do
+  key="$1"
+  case $key in
+    --reader_address)
+      reader_address="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --message_id)
+      message_id="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --slice_id)
+      slice_id="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --output_folder)
+      output_folder="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+# Check if mandatory arguments are provided
+if [ -z "$reader_address" ] || [ -z "$message_id" ] || [ -z "$slice_id" ] || [ -z "$output_folder" ]; then
+  echo "Usage: $0 --reader_address <reader_address> --message_id <message_id> --slice_id <slice_id> --output_folder <output_folder>"
+  exit 1
+fi
+
+# Run the commands with the provided arguments
+python3 client.py --handshake --reader_address "$reader_address"
+python3 client.py -gs --message_id "$message_id" --reader_address "$reader_address"
+python3 client.py -af --message_id "$message_id" --reader_address "$reader_address" --slice_id "$slice_id" --output_folder "$output_folder"
